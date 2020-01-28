12c8638f5899356441c0227599453184f55fb7e7
axwell <alex@dhost.ro>
2020-01-28 14:21:55 +0200

    Update version

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-docker image build -t olgac/rabbitmq:3.7.8-management .
-docker push olgac/rabbitmq:3.7.8-management
+docker image build -t axwell/rabbitmq:3.8.2-management .
+docker push axwell/rabbitmq:3.8.2-management
